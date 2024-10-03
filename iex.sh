5128a4b75969e59a7f114dd965de25518fdebe58
Matthew Wyatt <mattydev@gmail.com>
2024-10-03 10:51:00 -0700

    Initial commit of some helper scripts

---
+++ iex.sh
@@ -0,0 +1,2 @@
+#!/usr/bin/env bash
+MSYS_NO_PATHCONV=1 docker exec -it elixirpractice-base-1 "iex" $@
---
+++ shell.sh
@@ -0,0 +1,2 @@
+#!/usr/bin/env bash
+MSYS_NO_PATHCONV=1 docker compose exec -it base /bin/bash
