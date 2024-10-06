9147f54a70d05f8194313121788e7cfc88c3f312
Matthew Wyatt <mattydev@gmail.com>
2024-10-05 17:15:52 -0700

    Add the TodoServer code, including comments for myself.

--- code/ch05/todo_server.ex
+++ code/ch05/todo_server.ex
@@ -1,6 +1,87 @@
 # We put both modules in here because we don't get to mix until Ch 07.
 
 defmodule TodoServer do
+  # example of usage:
+  # iex(1)> ts = TodoServer.start()
+  # #PID<0.114.0>
+  # iex(2)> TodoServer.add_entry(ts, %{date: ~D[2024-10-05], title: "Task 1"})
+  # {:add_entry, %{date: ~D[2024-10-05], title: "Task 1"}}
+  # iex(3)> TodoServer.entries(ts)
+  # [%{id: 1, date: ~D[2024-10-05], title: "Task 1"}]
+  # (Note: see comment in the last process_message clause for why this is repeated)
+  # iex(4)> TodoServer.entries(ts)
+  # [%{id: 1, date: ~D[2024-10-05], title: "Task 1"}]
+  # iex(5)> TodoServer.add_entry(ts, %{date: ~D[2024-10-06], title: "Watch Arsenal win"})
+  # {:add_entry, %{date: ~D[2024-10-06], title: "Watch Arsenal win"}}
+  # iex(6)> TodoServer.entries(ts)
+  # [
+  #   %{id: 1, date: ~D[2024-10-05], title: "Task 1"},
+  #   %{id: 2, date: ~D[2024-10-06], title: "Watch Arsenal win"}
+  # ]
+  # iex(7)> TodoServer.entries(ts, ~D[2024-10-05])
+  # [%{id: 1, date: ~D[2024-10-05], title: "Task 1"}]
+
+  def start do
+    # I was wondering why this book uses a lambda whenever it calls spawn/1. I think it's
+    # because it's a little simpler to call spawn/1 and not spawn/3; see the top bit of
+    # https://hexdocs.pm/elixir/1.12/Process.html for more context. spawn/1 takes a 0-arity
+    # function, so if we want to pass args to it, we have to either jump through the hoop of
+    # spawn/3 or use a lambda.
+    spawn(fn -> loop(TodoList.new()) end)
+  end
+
+  def add_entry(todo_server, new_entry) do
+    send(todo_server, {:add_entry, new_entry})
+  end
+
+  def entries(todo_server, date) do
+    send(todo_server, {:entries, self(), date})
+    receive do
+      {:todo_entries, entries} -> entries
+    after
+      5000 -> {:error, :timeout}
+    end
+  end
+
+  def entries(todo_server) do
+    send(todo_server, {:entries, self()})
+    receive do
+      {:todo_entries, entries} -> entries
+    after
+      5000 -> {:error, :timeout}
+    end
+  end
+
+  # non-interface code below here
+  defp loop(todo_list) do
+    # Note once again that we use the "loop" function to manage state for the process
+    new_todo_list =
+      receive do
+        message -> process_message(todo_list, message)
+      end
+    loop(new_todo_list)
+  end
+
+  defp process_message(todo_list, {:add_entry, new_entry}) do
+    # implicitly returning the return value of TodoList.add_entry (the updated todo list):
+    TodoList.add_entry(todo_list, new_entry)
+  end
+
+  defp process_message(todo_list, {:entries, caller, date}) do
+    # This clause and the next one "return" values to the caller, AND have to maintain
+    # the state of the loop function:
+    send(caller, {:todo_entries, TodoList.entries(todo_list, date)})
+    todo_list
+  end
+
+  defp process_message(todo_list, {:entries, caller}) do
+    send(caller, {:todo_entries, TodoList.entries(todo_list)})
+    # The first time I tested this, I forgot to include this last line. This showed up as an
+    # error on the *second* time I called `TodoServer.entries(pid)`. Because the first time
+    # it was called, the state wasn't returned from this clause, it wiped the server state out.
+    todo_list
+  end
+
 end
 
 defmodule TodoList do
@@ -9,6 +90,8 @@ defmodule TodoList do
   #instantiates the TodoList struct. Note that the struct has the same
   #name as the module
   # def new(), do: %TodoList{}
+
+  # This is a strict superset of functionality over the above
   def new(entries \\ []) do
     Enum.reduce(
       entries,
