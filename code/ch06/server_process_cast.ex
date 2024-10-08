a09af4f4f5c23d8f4d0d8139fb6fee7a86a77cc8
Matthew Wyatt <mattydev@gmail.com>
2024-10-07 17:28:45 -0700

    Initial commit of server_process_cast.ex

---
+++ code/ch06/server_process_cast.ex
@@ -0,0 +1,69 @@
+# This module adapts the server_process module to use "casts" (asynchronous messages).
+defmodule ServerProcess do
+  def start(callback_module) do
+    # Pedagogically, why didn't the author use registered processes here?
+    spawn(fn ->
+      initial_state = callback_module.init()
+      loop(callback_module, initial_state)
+    end)
+  end
+
+  # call/2 and loop/2 should look very familiar after ch 05, as these are
+  # now well-established patterns.
+  def call(server_pid, request) do
+    send(server_pid, {:call, request, self()})
+    receive do
+      {:response, response} -> response
+    end
+  end
+
+  def cast(server_pid, request) do
+    send(server_pid, {:cast, request}) # don't need to pass self() since this is fire-and-forget
+  end
+
+  defp loop(callback_module, current_state) do
+    receive do
+      {:call, request, caller} ->
+        {response, new_state} = callback_module.handle_call(request, current_state)
+        send(caller, {:response, response})
+        loop(callback_module, new_state)
+      {:cast, request} ->
+        new_state = callback_module.handle_cast(request, current_state)
+        loop(callback_module, new_state)
+    end
+  end
+end
+
+# This is the "callback" module. It implements the specific hooks that the generic
+# server needs; in this case, that's init/0 and handle_call/2.
+defmodule KeyValueStore do
+  # interface functions
+  def start do
+    ServerProcess.start(KeyValueStore)
+  end
+
+  def put(pid, key, value) do
+    ServerProcess.cast(pid, {:put, key, value})
+  end
+
+  def get(pid, key) do
+    ServerProcess.call(pid, {:get, key})
+  end
+
+  # callback functions
+  def init do
+    %{}
+  end
+
+  def handle_call({:get, key}, state) do
+    {Map.get(state, key), state}
+  end
+
+  def handle_call({:put, key, value}, state) do
+    {:ok, Map.put(state, key, value)}
+  end
+
+  def handle_cast({:put, key, value}, state) do
+    Map.put(state, key, value)
+  end
+end
