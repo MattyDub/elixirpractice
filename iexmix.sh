91789d993248fbd19af8b6fda09fe300edad0cf2
Matthew Wyatt <mattydev@gmail.com>
2024-11-03 07:03:53 -0800

    Fix comment

--- iexmix.sh
+++ iexmix.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
-# USAGE: `./iex.sh [path to mix project]`
-# e.g. `./iex.sh ch07/todo`
+# USAGE: `./iexmix.sh [path to mix project]`
+# e.g. `./iexmix.sh ch07/todo`
 # NOTE that the `/code/code` is already prepended to the path
 MSYS_NO_PATHCONV=1 docker exec -it -w /code/code/$1 elixirpractice-base-1 sh -c "iex -S mix"
