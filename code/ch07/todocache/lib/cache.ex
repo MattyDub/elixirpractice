c524b31748c9b6686ff5b10d5d9e7a2b03b18160
Matthew Wyatt <mattydev@gmail.com>
2024-12-04 13:09:07 -0800

    Add comment and impls

--- code/ch07/todocache/lib/cache.ex
+++ code/ch07/todocache/lib/cache.ex
@@ -1,11 +1,14 @@
+# This module keeps a map of servers, and manages interactions with them.
 defmodule Todo.Cache do
   use GenServer
 
   # behaviour functions
+  @impl GenServer
   def init(_) do
     {:ok, %{}}
   end
 
+  @impl GenServer
   def handle_call({:server_process, todo_list_name}, _, todo_servers) do
     # This is the meat of the cache: fetches the pid if it exists, or creates
     # it and tracks it if it doesn't (and then returns the pid)
