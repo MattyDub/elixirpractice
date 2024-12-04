e39e4073aaa4bde38390ab1ff03cea808379d5c5
Matthew Wyatt <mattydev@gmail.com>
2024-12-04 13:09:18 -0800

    Add a test and some comments

--- code/ch07/todocache/test/todo_cache_test.exs
+++ code/ch07/todocache/test/todo_cache_test.exs
@@ -8,7 +8,18 @@ defmodule TodoCacheTest do
     {:ok, cache} = Todo.Cache.start()
     foo_pid = Todo.Cache.server_process(cache, "foo")
 
+    # NB that `assert` is also a macro. See the definition here:
+    # https://github.com/elixir-lang/elixir/blob/main-latest/lib/ex_unit/lib/ex_unit/assertions.ex#L131
     assert foo_pid != Todo.Cache.server_process(cache, "bar")
     assert foo_pid == Todo.Cache.server_process(cache, "foo")
+  end
+
+  test "to-do operations" do
+    {:ok, cache} = Todo.Cache.start()
+    alice = Todo.Cache.server_process(cache, "alice")
+    Todo.Server.add_entry(alice, %{date: ~D[2024-12-04], title: "Do the thing"})
+    entries = Todo.Server.entries(alice, ~D[2024-12-04])
+    # Note that we are using pattern matching here; we are "unpacking" only the fields we care about:
+    assert [%{date: ~D[2024-12-04], title: "Do the thing"}] = entries
   end
 end
