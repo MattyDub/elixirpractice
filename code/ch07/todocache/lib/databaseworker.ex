a803796e9751c35d7f5d5680b915bba1dee9b05e
Matthew Wyatt <mattydev@gmail.com>
2024-12-07 07:20:10 -0800

    Initial commit of Todo.DatabaseWorker module

---
+++ code/ch07/todocache/lib/databaseworker.ex
@@ -0,0 +1,56 @@
+# This is part of the exercise described on pg. 198-199. Persisting the data
+# via a process like this allows for synchronizing access to a particular
+# Todo list, as only this process will be able to access it. The Todo.Database
+# module calls this, which does the actual synchronization.
+defmodule Todo.DatabaseWorker do
+  use GenServer
+
+  @persist_folder "./persist"
+
+  # Note we don't use a :name key, as we need multiple of these workers.
+  def start(folder_name) do
+    GenServer.start(__MODULE__, folder_name)
+  end
+
+  # And also remember, future me, that if you don't register with a name,
+  # you need to pass in the pid that was returned from init/1.
+  def store(worker, key, data) do
+    GenServer.cast(worker, {:store, key, data})
+  end
+
+  def get(worker, key) do
+    GenServer.call(worker, {:get, key})
+  end
+
+  # The exercise didn't say to nest these under a folder, but I didn't want
+  # my filesystem getting cluttered with lots of little files all over the
+  # place, hence `db_folder`.
+  @impl GenServer
+  def init(folder_name) do
+    db_folder = Path.join(@persist_folder, folder_name)
+    File.mkdir_p!(db_folder)
+    {:ok, db_folder}
+  end
+
+  @impl GenServer
+  def handle_cast({:store, key, data}, db_folder) do
+    key
+    |> file_name(db_folder)
+    |> File.write!(:erlang.term_to_binary(data))
+
+    {:noreply, db_folder}
+  end
+
+  @impl GenServer
+  def handle_call({:get, key}, _, db_folder) do
+    data = case File.read(file_name(key, db_folder)) do
+      {:ok, contents} -> :erlang.binary_to_term(contents)
+      _ -> nil
+    end
+    {:reply, data, db_folder}
+  end
+
+  defp file_name(key, db_folder) do
+    Path.join(db_folder, to_string(key))
+  end
+end
