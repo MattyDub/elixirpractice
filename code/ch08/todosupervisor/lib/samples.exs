7445e6918ecde884ac7d010a5f7f545914d224eb
Matthew Wyatt <mattydev@gmail.com>
2024-12-18 15:28:21 -0800

    Initial commit of some sample code/notes

---
+++ code/ch08/todosupervisor/lib/samples.exs
@@ -0,0 +1,8 @@
+# This file contains some code snippets to show how to do some things.
+# The intent is more to copy and paste these into iex than to run this file as a script.
+
+# Supervisor start/kill/restart example:
+Supervisor.start_link([Todo.Cache], strategy: :one_for_one) # :one_for_one restarts the process 1:1 when it is shut down
+cache_pid = Process.whereis(Todo.Cache) # returns the PID for Todo.Cache
+Process.exit(cache_pid, :kill) # :kill exits even if the process is trapping exits
+Process.whereis(Todo.Cache) # you can see this returns a different PID than was returned earlier
